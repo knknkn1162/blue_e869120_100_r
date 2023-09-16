72c75d475e722c9e1f06052a5cefc6b06ab3030a
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-16 16:41:35 +0900

    solve 004 in R

---
+++ 004/01.R
@@ -0,0 +1,24 @@
+options(scipen=100)
+#conn <- file("stdin", open="r")
+conn <- stdin()
+
+inputs <- readLines(conn, n=1) |> str_split_1(" ") |> parse_number()
+N <- inputs[1]; M <- inputs[2]
+
+A <- read.table(conn) |>
+  as_tibble()
+
+expand_grid(T1 = 1:M, T2 = 1:M) |>
+  filter(T1 < T2) |>
+  rowwise() |>
+  group_map(\(sdf,key) A |>
+    select(sdf |> unlist()) |>
+    rowwise() |>
+    mutate(v = max(c_across(everything()))) |>
+    ungroup() |>
+    summarize(sum(v)) |>
+    pull()
+  ) |>
+  unlist() |>
+  max() |>
+  cat()
